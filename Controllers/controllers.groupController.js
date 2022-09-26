cae42ed5f28d28e44154f982b0c3db52be74aded
Nguyen Xuan Khoa <70152642+bill-0808@users.noreply.github.com>
2022-09-26 17:15:54 +0700

    Revert "express server"
    This reverts commit 4563da633f74c2a6719d7878129a84f8336f57a6.

--- Controllers/controllers.groupController.js
+++
@@ -1,9 +0,0 @@
-const { mygroup } = require('../Models/models.group');
-
-function getAllGroup(req, res) {
-    res.status(200).json(mygroup);
-}
-
-module.exports = {
-    getAllGroup
-}
--- Models/models.group.js
+++
@@ -1,5 +0,0 @@
-var mygroup = [{ id: '19110380', name: 'Nguyen Xuan Khoa' }]
-
-module.exports = {
-    mygroup
-}
